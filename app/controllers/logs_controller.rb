cb90c569e3268dce82e51d7d08115a10488b6e0f
bmckibben <github@literallight.com>
2023-06-11 16:08:59 -0400

    completed turbo drive section of tutorial

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,9 +1,51 @@
 class LogsController < InheritedResources::Base
 
+  before_action :set_log, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @logs = Log.all
+  end
+
+  def show
+  end
+
+  def new
+    @log = Log.new(user_id: current_user.id)
+  end
+
+  def create
+    @log = Log.new(log_params)
+
+    if @log.save
+      redirect_to logs_path, notice: "Log was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @log.update(log_params)
+      redirect_to logs_path, notice: "Log was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @log.destroy
+    redirect_to logs_path, notice: "Log was successfully destroyed."
+  end
+
   private
 
-    def log_params
-      params.require(:log).permit(:user_id, :start_at, :end_at, :notes)
-    end
+  def set_log
+    @log = Log.find(params[:id])
+  end
 
+  def log_params
+    params.require(:log).permit(:user_id, :start_at, :end_at, :notes, :description, :task_id, "notes")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User <ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
+
+ def full_name
+    self.login
+ end
 end
